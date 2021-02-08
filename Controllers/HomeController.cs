0a9a038cacfc35579c1d6ba66a36373b2b6ee5df
Thomas Sorteberg <tsorteberg@dmacc.edu>
2021-02-07 23:01:47 -0600

    Fourth commit. Added home controller and updated index view.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using MultiPageWebApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿/***************************************************************
+* Name        : HomeController.cs
+* Author      : Tom Sorteberg
+* Created     : 02/07/2021
+* Course      : CIS 174
+* Version     : 1.0
+* OS          : Windows 10 Pro, Visual Studio Community 2019
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Module 2 Topic 3
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access 
+* to my program.         
+***************************************************************/
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiPageWebApp.Models;
 
 namespace MultiPageWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        // Context class private attribute.
+        private ContactContext context { get; set; }
+        
+        // Primary contructor.
+        public HomeController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+        // Index action.
         public IActionResult Index()
         {
-            return View();
+            var contacts = context.Contacts.OrderBy(m => m.Name).ToList();
+            return View(contacts);
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,17 @@
+/***************************************************************
+* Name        : Startup.cs
+* Author      : Tom Sorteberg
+* Created     : 02/07/2021
+* Course      : CIS 174
+* Version     : 1.0
+* OS          : Windows 10 Pro, Visual Studio Community 2019
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : Module 2 Topic 3  
+* I have not used unauthorized source code, either modified or
+* unmodified. I have not given other fellow student(s) access
+* to my program.
+***************************************************************/
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
